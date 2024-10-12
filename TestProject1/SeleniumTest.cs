143d3eba675467c0ea0ae17e6731517321622e71
Riko435 <vizhva2014@gmail.com>
2024-10-12 15:51:43 +0300

    Add project files.

---
+++ TestProject1/SeleniumTest.cs
@@ -0,0 +1,78 @@
+//Inside SeleniumTest.cs
+
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium.Firefox;
+
+using System;
+
+using System.Collections.ObjectModel;
+
+using System.IO;
+
+namespace SeleniumCsharp
+
+{
+    [AllureNUnit]
+    public class Tests
+
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+
+        public void Setup()
+
+        {
+
+            //Below code is to get the drivers folder path dynamically.
+
+            //You can also specify chromedriver.exe path dircly ex: C:/MyProject/Project/drivers
+
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            //Creates the ChomeDriver object, Executes tests on Google Chrome
+
+            driver = new ChromeDriver(path + @"\drivers\");
+
+            //If you want to Execute Tests on Firefox uncomment the below code
+
+            // Specify Correct location of geckodriver.exe folder path. Ex: C:/Project/drivers
+
+            //driver= new FirefoxDriver(path + @"\drivers\");
+
+        }
+        [AllureSuite("Verify Logo")]
+        [Test]
+
+        public void verifyLogo()
+
+        {
+
+            driver.Navigate().GoToUrl("https://www.browserstack.com/");
+
+            Assert.That(driver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/div/div/a/img")).Displayed);
+
+        }
+
+
+        [OneTimeTearDown]
+
+        public void TearDown()
+
+        {
+
+            driver.Quit();
+            driver.Dispose();
+
+        }
+
+    }
+
+}
